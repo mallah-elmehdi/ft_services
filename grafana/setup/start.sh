e308c088d03d7018218831b0f46c23837f33d84d
mallah-elmehdi <elmehdimallah@gmail.com>
2021-05-03 11:11:56 +0000

    slfk

--- grafana/setup/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd ./grafana-7.5.5/bin/ && ./grafana-server
-tail -f /dev/null
--- src/nginx/setup/setup.sh
+++ src/nginx/setup/setup.sh
@@ -4,6 +4,8 @@ apk add nginx
 apk add openrc
 rc-status
 touch /run/openrc/softlevel
+mkdir /etc/nginx/ssl
+mv ~/thekey.* /etc/nginx/ssl
 mv ~/default.conf /etc/nginx/conf.d/
 mv ~/index.html /var/www
 mv ~/style.css /var/www
