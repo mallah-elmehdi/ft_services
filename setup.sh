ab6fdd17dff34a3eeabeffcf25382bb8784371a3
mallah-elmehdi <elmehdimallah@gmail.com>
2021-05-03 10:48:11 +0000

    let do this

--- setup.sh
+++ setup.sh
@@ -6,8 +6,10 @@
 # 192.168.99.100
 # E
 
-kubectl delete deployment grafana influxdb  mysql nginx  phpmyadmin  wordpress 
-kubectl delete service grafana-service influxdb-service mysql-service nginx-service phpmyadmin-service wordpress-service 
+kubectl delete deployment grafana mysql nginx phpmyadmin wordpress 
+kubectl delete service grafana-service mysql-service nginx-service phpmyadmin-service wordpress-service
+
+docker rmi -f $(docker images -a -q)
 
 docker build -t nginx "src/nginx"
 docker build -t phpmyadmin "src/phpmyadmin"
--- src/nginx/setup/setup.sh
+++ src/nginx/setup/setup.sh
@@ -5,5 +5,7 @@ apk add openrc
 rc-status
 touch /run/openrc/softlevel
 mv ~/default.conf /etc/nginx/conf.d/
+mkdir /etc/nginx/ssl/
+mv ~/thekey.* /etc/nginx/ssl/
 mv ~/index.html /var/www
 mv ~/style.css /var/www
